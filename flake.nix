6066f6b3b9561848da616db4e1bd76e31c29274b
Pebble Real <pawel.chesterfield@gmail.com>
2025-05-08 08:12:40 +0100

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # The host with the hostname `my-nixos` will use this configuration
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+
+}
