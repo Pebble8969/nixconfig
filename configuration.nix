492cc970b60494209264b643e7f4a4f373dc961f
Pebble Real <pawel.chesterfield@gmail.com>
2025-05-15 18:59:32 +0100

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -46,8 +46,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "gb";
@@ -61,7 +61,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -103,6 +103,7 @@
 
   # Install firefox.
   programs.firefox.enable = true;
+  programs.hyprland.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -124,6 +125,14 @@
     git
     vscode
     gcc
+    alacritty
+    waybar
+    wofi
+    hyprshot
+    hyprpaper
+    hyprlock
+    hyprlang
+    SwayNotificationCenter
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -166,39 +175,38 @@
     enable = true;
   };
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+ #  # Load nvidia driver for Xorg and Wayland
+ # services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
+ # hardware.nvidia = {
 
-    # Modesetting is required.
-    modesetting.enable = true;
+ #   # Modesetting is required.
+ #   modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
+ #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+ #   # Enable this if you have graphical corruption issues or application crashes after waking
+ #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+ #   # of just the bare essentials.
+ #   powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+ #   # Fine-grained power management. Turns off GPU when not in use.
+ #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+ #   powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    open = false;
+ #   # Use the NVidia open source kernel module (not to be confused with the
+ #   # independent third-party "nouveau" open source driver).
+ #   # Support is limited to the Turing and later architectures. Full list of 
+ #   # supported GPUs is at: 
+ #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+ #   # Only available from driver 515.43.04+
+ #   open = false;
 
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+ #   # Enable the Nvidia settings menu,
+ #   nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+ #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+ #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+ # };
 
   programs.steam = {
   enable = true;
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "99p with flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
 
     #home-manager = {
     #  url = "github:nix-community/home-manager";
